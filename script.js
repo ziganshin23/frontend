7b73624e4174c2430b1a2d5cae911e6d7a838c06
ziganshin23 <ziganshinns@gmail.com>
2018-02-25 12:29:47 +0300

    'Initial'

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 'use strict';
-
-const http = require('http');
+//script.js
 const fs = require('fs');
 
-const server = http.createServer(function (request, response){
-    console.log(request.method, request.url);
-    if(request.url == '/style.css'){
-        const css = fs.readFileSync('style.css', 'utf8');
-        response.end(css);
-    }else {
-        const html = fs.readFileSync('index.html', 'utf8');
-        response.end(html);
-    }
-});
-server.listen(3000);
-console.log('Server started!');
+const text = fs.readFileSync('script.js', 'utf8');
+console.log(text);
